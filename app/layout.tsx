9955d600f1da6abf6a6a444f1760100a37b4abb8
Jay Cunningham <j@crosscompiled.com>
2026-02-10 21:35:14 -0500

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,20 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9K87YWBE7P" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9K87YWBE7P');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen bg-background-secondary">
           {/* Header */}
